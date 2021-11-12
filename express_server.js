da670d47895ba237aa01ddf9d26a58f71bdf37db
Abdullah Khan <abdullahshamskhan@gmail.com>
2021-11-12 20:59:44 +0000

    Setup bcryptjs

--- express_server.js
+++ express_server.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 8080;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
