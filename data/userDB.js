6d97d6b2733c6ae382daa875ed769622288d5455
Abdullah Khan <abdullahshamskhan@gmail.com>
2021-11-12 03:37:40 +0000

    Refactored code, moved database into seperate directory

---
+++ data/userDB.js
@@ -0,0 +1,19 @@
+const users = {
+  "aJ48lW": {
+    id: "aJ48lW",
+    email: "aaa@abc.com",
+    password: "asdfg"
+  }, 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+module.exports = users;
